28d183a9e0782ab43f59a36d1de3ce1caf3054b2
andrey <avran2002@gmail.com>
2024-10-26 07:36:49 +0300

    added logger

---
+++ logger/logger.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/avran02/authentication/internal/config"
+)
+
+func Setup(config config.Server) {
+	var ll slog.Leveler
+	var isDefaultLogLevel bool
+	switch config.LogLevel {
+	case "debug":
+		ll = slog.LevelDebug
+	case "info":
+		ll = slog.LevelInfo
+	case "warn":
+		ll = slog.LevelWarn
+	case "error":
+		ll = slog.LevelError
+	default:
+		isDefaultLogLevel = true
+		ll = slog.LevelInfo
+	}
+
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: ll,
+	}))
+
+	slog.SetDefault(log)
+
+	if isDefaultLogLevel {
+		slog.Warn("Logger using default value", "log level", ll)
+		return
+	}
+	slog.Info("Logger", "log level", ll)
+}
